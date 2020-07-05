c07b351379b6621e407485b7eb3e8be1a23c523c
Siarhei Pirahou <sergserg353@gmail.com>
2020-07-05 18:25:18 +0300

    add vue router

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -6,7 +6,7 @@ import App from 'pages/App.vue'
 import store from 'store/store'
 import { connect } from './util/ws'
 import 'vuetify/dist/vuetify.min.css'
-
+import router from 'router/router'
 
 if (frontendData.profile) {
     connect();
@@ -18,5 +18,6 @@ new Vue({
     vuetify : new Vuetify(),
     el: '#app',
     store,
+    router,
     render: a => a(App)
 })
---
+++ src/main/resources/static/js/pages/Auth.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>Please login or register
+        <a href="/login">Google</a>
+    </v-container>
+</template>
+
+<script>
+    export default {
+        name: "Auth"
+    }
+</script>
+
+<style scoped>
+
+</style>
